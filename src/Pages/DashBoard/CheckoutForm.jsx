d5c08a2d900862c16f4b4a3a7f78a1fc9cf9d642
Mehraj Hasan Riaj <mehrajhasanriajcse@gmail.com>
2025-07-08 19:56:16 +0600

    add payment system with functionality where update unpaid to paid and added to transaction history if success

--- src/Pages/DashBoard/CheckoutForm.jsx
+++ src/Pages/DashBoard/CheckoutForm.jsx
@@ -4,28 +4,32 @@ import useAxiosSecure from "../../hooks/useAxiosSecure";
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router";
 import Loading from "../Loading/Loading";
+import useAuth from "../../hooks/useAuth";
 
 const CheckoutForm = () => {
+  const { user } = useAuth();
   const stripe = useStripe();
   const elements = useElements();
-  const [error , setError] = useState('');
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
   const axiosSecure = useAxiosSecure();
-  const {id} = useParams();
+  const { id } = useParams();
 
-  const {data : parcelInfo = {} , isPending} = useQuery({
-    queryKey: ['parcels', id],
-    queryFn: async() =>{
-        const res = await axiosSecure.get(`parcels/${id}`);
-        return res.data;
-    }
-  })
-  if(isPending){
-    return <Loading></Loading>
+  const { data: parcelInfo = {}, isPending } = useQuery({
+    queryKey: ["parcels", id],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`parcels/${id}`);
+      return res.data;
+    },
+  });
+  if (isPending) {
+    return <Loading></Loading>;
   }
   console.log(parcelInfo);
   const amount = parcelInfo?.cost;
+  const amountInCents = amount * 100;
 
-  const handleSubmit = async(e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     if (!stripe || !elements) {
@@ -37,20 +41,53 @@ const CheckoutForm = () => {
       return;
     }
 
-    const {error , paymentMethod} = await stripe.createPaymentMethod({
-        type: 'card',
-        card
-    })
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
 
-    if(error){
-        setError(error.message);
-        console.log(error);
-    }
-    else{
-        setError('');
-         console.log(paymentMethod);
+    if (error) {
+      setError(error.message);
+      console.log(error);
+    } else {
+      setError("");
+      console.log(paymentMethod);
     }
 
+    const { data } = await axiosSecure.post("/create-payment-intent", {
+      amount: amountInCents,
+    });
+
+    console.log(data);
+
+    const result = await stripe.confirmCardPayment(data.clientSecret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: "Customer Name",
+        },
+      },
+    });
+
+    if (result.error) {
+      setError(result.error.message);
+    } else {
+      if (result.paymentIntent.status === "succeeded") {
+        // Save order/payment data to DB
+        const paymentInfo = {
+          transactionId: result.paymentIntent.id,
+          amount: amountInCents,
+          email: user?.email,
+          parcelId: parcelInfo._id,
+          paymentMethod: result.paymentIntent.payment_method_types,
+        };
+
+        await axiosSecure.post("/payments", paymentInfo).then((res) => {
+          setError("");
+          setSuccess("Payment successful!");
+        });
+      }
+    }
   };
   return (
     <div>
@@ -66,9 +103,8 @@ const CheckoutForm = () => {
         >
           Pay ${amount}
         </button>
-        {
-            error && <p className="text-red-600">{error}</p>
-        }
+        {error && <p className="text-red-600">{error}</p>}
+        {success && <p className="text-green-600 mt-4">{success}</p>}
       </form>
     </div>
   );
